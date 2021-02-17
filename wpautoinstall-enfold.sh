3e33aba23e57f2ac6d84d62ac8ee511ee41e562a
vsapountzis <evangelos@sapountzis.gr>
2021-02-17 12:54:43 +0200

    Create wpautoinstall-enfold.sh

---
+++ wpautoinstall-enfold.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+wp theme install https://github.com/prome-media/wpautoinstall-enfold/raw/main/enfold.zip
+wp theme install https://kriesi.at/documentation/enfold/wp-content/uploads/sites/22/2020/11/enfold-child.zip --activate
+wp theme delete twentynineteen
+wp theme delete twentytwenty
+wp plugin delete akismet
+wp plugin delete hello
+wp plugin install updraftplus --activate
+wp plugin install wordfence --activate
+wp plugin install classic-editor --activate
+wp plugin install duplicate-post --activate
+
