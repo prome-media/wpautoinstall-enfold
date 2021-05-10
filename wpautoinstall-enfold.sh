7d96aaaedba3cc3354572f56eb166fb45e2aed18
vsapountzis <evangelos@sapountzis.gr>
2021-05-10 12:53:46 +0300

    Added a way to import the SG optimizer settings line by line, instead ofhash import errors, due to errors

--- wpautoinstall-enfold.sh
+++ wpautoinstall-enfold.sh
@@ -27,7 +27,15 @@ read -r -p "Is this an SG hosted website? If yes, this will install SG Optimizer
 read -r -p "Do you want to auto configure the SG Optimizer settings for this website? [y/N] " response
 
     if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
-        wp sg settings import -hash=eyJzdXBlcmNhY2hlcl9wZXJtaXNzaW9ucyI6MSwiZW5hYmxlX2NhY2hlIjoxLCJhdXRvZmx1c2hfY2FjaGUiOjEsImVuYWJsZV9tZW1jYWNoZWQiOjEsInNzbF9lbmFibGVkIjoxLCJvcHRpbWl6ZV9odG1sIjoxLCJvcHRpbWl6ZV9qYXZhc2NyaXB0IjoxLCJvcHRpbWl6ZV9qYXZhc2NyaXB0X2FzeW5jIjoxLCJvcHRpbWl6ZV9jc3MiOjEsImNvbWJpbmVfY3NzIjowLCJyZW1vdmVfcXVlcnlfc3RyaW5ncyI6MSwiZGlzYWJsZV9lbW9qaXMiOjEsIm9wdGltaXplX2ltYWdlcyI6MSwiZW5hYmxlX2d6aXBfY29tcHJlc3Npb24iOjAsImVuYWJsZV9icm93c2VyX2NhY2hpbmciOjAsImxhenlsb2FkX21vYmlsZSI6MSwiYXN5bmNfamF2YXNjcmlwdF9leGNsdWRlIjpbImpxdWVyeS1taWdyYXRlIiwianF1ZXJ5IiwianF1ZXJ5LWNvcmUiXSwiZXhjbHVkZWRfbGF6eV9sb2FkX2NsYXNzZXMiOlsic2tpcC1sYXp5Il0sImxhenlsb2FkX2lmcmFtZXMiOjEsImxhenlsb2FkX3ZpZGVvcyI6MSwiY29tYmluZV9qYXZhc2NyaXB0IjowLCJvcHRpbWl6ZV93ZWJfZm9udHMiOjEsImxhenlsb2FkX2ltYWdlcyI6MSwiZml4X2luc2VjdXJlX2NvbnRlbnQiOjEsImRhdGFiYXNlX29wdGltaXphdGlvbiI6MSwibWluaWZ5X2phdmFzY3JpcHRfZXhjbHVkZSI6W119
+        #wp sg settings import -hash=eyJzdXBlcmNhY2hlcl9wZXJtaXNzaW9ucyI6MSwiZW5hYmxlX2NhY2hlIjoxLCJhdXRvZmx1c2hfY2FjaGUiOjEsImVuYWJsZV9tZW1jYWNoZWQiOjEsInNzbF9lbmFibGVkIjoxLCJvcHRpbWl6ZV9odG1sIjoxLCJvcHRpbWl6ZV9qYXZhc2NyaXB0IjoxLCJvcHRpbWl6ZV9qYXZhc2NyaXB0X2FzeW5jIjoxLCJvcHRpbWl6ZV9jc3MiOjEsImNvbWJpbmVfY3NzIjowLCJyZW1vdmVfcXVlcnlfc3RyaW5ncyI6MSwiZGlzYWJsZV9lbW9qaXMiOjEsIm9wdGltaXplX2ltYWdlcyI6MSwiZW5hYmxlX2d6aXBfY29tcHJlc3Npb24iOjAsImVuYWJsZV9icm93c2VyX2NhY2hpbmciOjAsImxhenlsb2FkX21vYmlsZSI6MSwiYXN5bmNfamF2YXNjcmlwdF9leGNsdWRlIjpbImpxdWVyeS1taWdyYXRlIiwianF1ZXJ5IiwianF1ZXJ5LWNvcmUiXSwiZXhjbHVkZWRfbGF6eV9sb2FkX2NsYXNzZXMiOlsic2tpcC1sYXp5Il0sImxhenlsb2FkX2lmcmFtZXMiOjEsImxhenlsb2FkX3ZpZGVvcyI6MSwiY29tYmluZV9qYXZhc2NyaXB0IjowLCJvcHRpbWl6ZV93ZWJfZm9udHMiOjEsImxhenlsb2FkX2ltYWdlcyI6MSwiZml4X2luc2VjdXJlX2NvbnRlbnQiOjEsImRhdGFiYXNlX29wdGltaXphdGlvbiI6MSwibWluaWZ5X2phdmFzY3JpcHRfZXhjbHVkZSI6W119
+        wp sg memcached enable
+        wp sg optimize html enable
+        wp sg optimize js enable
+        wp sg optimize css enable
+        wp sg optimize emojis enable
+        wp sg optimize images enable
+        wp sg optimize lazyload enable
+        wp sg optimize database-optimization enable
     fi
 
 # Media Cloud Plugin Installation and Activation
