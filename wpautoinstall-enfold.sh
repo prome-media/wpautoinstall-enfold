2c36a9916e9f7fec921e2d349f66e31e5e6e0eaa
vsapountzis <evangelos@sapountzis.gr>
2021-05-18 21:22:36 +0300

    Added TODOs for next steps/improvements of the script

--- wpautoinstall-enfold.sh
+++ wpautoinstall-enfold.sh
@@ -28,6 +28,7 @@ read -r -p "Do you want to auto configure the SG Optimizer settings for this web
 
     if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
         #wp sg settings import -hash=eyJzdXBlcmNhY2hlcl9wZXJtaXNzaW9ucyI6MSwiZW5hYmxlX2NhY2hlIjoxLCJhdXRvZmx1c2hfY2FjaGUiOjEsImVuYWJsZV9tZW1jYWNoZWQiOjEsInNzbF9lbmFibGVkIjoxLCJvcHRpbWl6ZV9odG1sIjoxLCJvcHRpbWl6ZV9qYXZhc2NyaXB0IjoxLCJvcHRpbWl6ZV9qYXZhc2NyaXB0X2FzeW5jIjoxLCJvcHRpbWl6ZV9jc3MiOjEsImNvbWJpbmVfY3NzIjowLCJyZW1vdmVfcXVlcnlfc3RyaW5ncyI6MSwiZGlzYWJsZV9lbW9qaXMiOjEsIm9wdGltaXplX2ltYWdlcyI6MSwiZW5hYmxlX2d6aXBfY29tcHJlc3Npb24iOjAsImVuYWJsZV9icm93c2VyX2NhY2hpbmciOjAsImxhenlsb2FkX21vYmlsZSI6MSwiYXN5bmNfamF2YXNjcmlwdF9leGNsdWRlIjpbImpxdWVyeS1taWdyYXRlIiwianF1ZXJ5IiwianF1ZXJ5LWNvcmUiXSwiZXhjbHVkZWRfbGF6eV9sb2FkX2NsYXNzZXMiOlsic2tpcC1sYXp5Il0sImxhenlsb2FkX2lmcmFtZXMiOjEsImxhenlsb2FkX3ZpZGVvcyI6MSwiY29tYmluZV9qYXZhc2NyaXB0IjowLCJvcHRpbWl6ZV93ZWJfZm9udHMiOjEsImxhenlsb2FkX2ltYWdlcyI6MSwiZml4X2luc2VjdXJlX2NvbnRlbnQiOjEsImRhdGFiYXNlX29wdGltaXphdGlvbiI6MSwibWluaWZ5X2phdmFzY3JpcHRfZXhjbHVkZSI6W119
+        #TODO: Account for page exclusions for cache (WC endpoints for example)
         wp sg memcached enable
         wp sg optimize html enable
         wp sg optimize js enable
@@ -51,6 +52,10 @@ read -r -p "Do you want to update the permalink structure? [y/N] " response
     if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
         wp rewrite structure '/%postname%'
     fi
+
+# Import Basic Enfold Child Settings
+# TODO: Use wp db prefix to get Database prefix and assign it to a variable
+# TODO: Use wp db query to import settings 
 
 # Script Cleanup
 read -r -p " All done! Do you want to delete the installation script? [y/N] " response
